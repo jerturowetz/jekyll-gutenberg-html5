87a4ad3214e346f70cf386d40f9604c3e4f94342
Jeremy Turowetz <j.turowetz@gmail.com>
2018-07-04 16:25:47 -0400

    add vendor script for chrome mobile vh bug fix

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+// // Example code for employing the chrome mobile vh units fix
+// var vhFixOptions = [
+//   {
+//     selector: '.Bears', // Mandatory, CSS selector
+//     vh: 150,  // Mandatory, height in vh units
+//   },
+//   {
+//     selector: '.Foxes',
+//     vh: 50
+//   },
+//   {
+//     selector: '.Horses',
+//     vh: 100
+//   }
+// ];
+
+// var vhFix = new VHChromeFix( vhFixOptions );
