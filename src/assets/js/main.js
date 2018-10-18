d63a487ea866f34c66c4519c52b123b8e325ab62
Jeremy Turowetz <j.turowetz@gmail.com>
2018-10-18 13:40:53 -0400

    merge from bkdsn & polyglot

---
+++ src/assets/js/main.js
@@ -0,0 +1,40 @@
+function menuToggle(x) {
+
+    var y = document.getElementsByClassName( 'dropdown-menu' );
+    var dropdownMenu = y[0];
+
+    x.classList.toggle( 'dropdown-open' );
+    dropdownMenu.classList.toggle( 'dropdown-open' );
+
+}
+
+
+function closeDropDownMenu() {
+
+    var dropdowns = document.getElementsByClassName( 'dropdown-menu' );
+    var dropdownMenu = dropdowns[0];
+
+    var toggles = document.getElementsByClassName( 'topbar__dropdown-toggle' );
+    var toggle = toggles[0];
+
+    dropdownMenu.classList.remove( 'dropdown-open' );
+    toggle.classList.remove( 'dropdown-open' );
+}
+
+
+function scrollToTarget(e) {
+    var dest = $( e ).attr( 'href' );
+    var new_position = $( dest ).offset();
+    $('html, body').stop().animate({ scrollTop: new_position.top }, 1000);
+    closeDropDownMenu();
+    e.preventDefault();
+}
+
+
+var vhFixOptions = [
+    {
+        selector: '.section--hero',
+        vh: 94,
+    }
+];
+var vhFix = new VHChromeFix( vhFixOptions );
